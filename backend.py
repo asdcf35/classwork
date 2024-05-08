b6fef6848a102991cdd5ccc1fc6305433d88d2e9
asdcf35 <prann1234556@gmail.com>
2024-05-08 02:08:04 +0000

    May 7, 2024, 10:08 PM

--- backend.py
+++ backend.py
@@ -2,11 +2,23 @@ from math import *
 import os
 import subprocess
 from rich.console import Console
+
 ask = input("Global 3.10?*(y/n)")
-if ask == 'y':
-    subprocess.run(["C:\\'Program Files'\\Python310\\python.exe","-m",'pip','install','pandas','colorama'])
+if ask == "y":
+    subprocess.run(
+        [
+            "C:\\'Program Files'\\Python310\\python.exe",
+            "-m",
+            "pip",
+            "install",
+            "pandas",
+            "colorama",
+        ]
+    )
 
 import pandas as pd
+
+
 # rides
 class Ride:
     def __init__(self, name, age_range, desc, working) -> None:
@@ -32,12 +44,17 @@ def rides_from_file(filename="rides.csv") -> dict[str, Ride]:
         the filename or object that you wish to use(must work with read_csv)
     """
     dataframe = pd.read_csv(filename)
-    dataframe.loc[:, 'Max'] = dataframe.loc[:,'Max'].fillna(999999999)
-    dataframe.loc[:, 'Min'] = dataframe.loc[:,'Min'].fillna(0)
-    dataframe = dataframe.to_dict('records')
+    dataframe.loc[:, "Max"] = dataframe.loc[:, "Max"].fillna(999999999)
+    dataframe.loc[:, "Min"] = dataframe.loc[:, "Min"].fillna(0)
+    dataframe = dataframe.to_dict("records")
     rides = {}
     for dictionary in dataframe:
-        rides[dictionary["Name"]] = Ride(dictionary['Name'],(dictionary['Min'], dictionary['Max']), dictionary['Description'], dictionary['Working'])
+        rides[dictionary["Name"]] = Ride(
+            dictionary["Name"],
+            (dictionary["Min"], dictionary["Max"]),
+            dictionary["Description"],
+            dictionary["Working"],
+        )
     return rides
 
 
@@ -54,11 +71,18 @@ class Restaurant:
         self.name = name
         self.open = open
         dataframe = pd.read_csv(filename)
-        self.item_names = tuple(dataframe.xs('Items'))
+        self.item_names = tuple(dataframe.xs("Items"))
         self.items = [
-            Food_Item(self.item_names[i], dataframe.loc[i, 'Price'],
-                      dataframe.loc[i, 'Amount Left'])
-            for i in range(len(self.item_names))
+            Food_Item(
+                self.item_names[i],
+                dataframe.loc[i, "Price"],
+                dataframe.loc[i, "Amount Left"],
+            ) for i in range(len(self.item_names))
         ]
-    
-    def display()
+
+    def display_foods(self, console: Console, discount=0) -> None:
+        console.print(f"Welcome to {self.name}\n\n", justify="center")
+        console.print(f"Menu", justify="center")
+        console.print(f'{"":-^24}', justify='center')
+        for food_item in self.items: 
+            print()
