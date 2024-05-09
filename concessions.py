b1302fa6fe33ab37de3f35257ae3063764f5456d
asdcf35 <prann1234556@gmail.com>
2024-05-09 00:06:32 +0000

    May 8, 2024, 8:06 PM

--- concessions.py
+++ concessions.py
@@ -4,23 +4,26 @@ from rich.console import Console
 
 
 def main():
-    console = Console()
-    restaurant_files = ["Burger City.csv", "Bread Bitez.csv"]
-    restaurants = {}
-    for restaurant_file in restaurant_files:
-        restaurants[restaurant_file[:-4]] = backend.Restaurant(
-            restaurant_file[:-4], True, restaurant_file
-        )
-
-    for index, restaurant in enumerate(restaurants.keys(), 1):
-        print(f"{index}, {restaurant}")
-    number = input("Enter one of the restaurants(number only), or q to quit")
-    number = "q" if number == "q" else int(number) - 1
-    restaurant_selected = list(restaurants.keys())[number]
-    restaurants[restaurant_selected].display_foods(console)
-    console.print("\n\nEnter q to go back to the restaurants", justify="center")
     while True:
-        if msvcrt.kbhit():
-            key = msvcrt.getch().decode("ASCII")
-            if key == "q":
-                break
+        console = Console()
+        restaurant_files = ["Burger City.csv", "Bread Bitez.csv"]
+        restaurants = {}
+        for restaurant_file in restaurant_files:
+            restaurants[restaurant_file[:-4]] = backend.Restaurant(
+                restaurant_file[:-4], True, restaurant_file
+            )
+
+        for index, restaurant in enumerate(restaurants.keys(), 1):
+            print(f"{index}, {restaurant}")
+        number = input("Enter one of the restaurants(number only), or q to quit")
+        number = "q" if number == "q" else int(number) - 1
+        if number == "q":
+            break
+        restaurant_selected = list(restaurants.keys())[number]
+        restaurants[restaurant_selected].display_foods(console)
+        console.print("\n\nEnter q to go back to the restaurants", justify="center")
+        while True:
+            if msvcrt.kbhit():
+                key = msvcrt.getch().decode("ASCII")
+                if key == "q":
+                    break
