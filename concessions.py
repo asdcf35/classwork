ec9eebe64058d2b69d450c1e887cfcc23ff63791
asdcf35 <prann1234556@gmail.com>
2024-05-09 20:24:13 +0000

    May 9, 2024, 4:24 PM

--- concessions.py
+++ concessions.py
@@ -28,6 +28,7 @@ def main():
         else:
             restaurants[restaurant_selected].display_foods(console)
             console.print("\n\nEnter q to go back to the restaurants", justify="center")
+
             while True:
                 if msvcrt.kbhit():
                     key = msvcrt.getch().decode("ASCII")
