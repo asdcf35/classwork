2382725074bb6170dd1e1d12aac22d2ba1ec0921
asdcf35 <prann1234556@gmail.com>
2024-05-11 03:36:19 +0000

    May 10, 2024, 11:36 PM

--- concessions.py
+++ concessions.py
@@ -3,20 +3,28 @@ import msvcrt
 from rich.console import Console
 import os
 
+
 def main():
+    """This is the concession's stand, where there will be """
     while True:
+        # clear system
         os.system('cls')
         console = Console()
 
-        restaurant_files = ["Burger City.csv", "Bread Bitez.csv"]
+        restaurant_files = [
+            "Burger City.csv", "Bread Bitez.csv", "Biryani Garden.csv"
+        ]
+
         restaurants = {}
         for restaurant_file in restaurant_files:
-            restaurants[restaurant_file[:-4]] = backend.Restaurant(restaurant_file[:-4], True, restaurant_file)
+            restaurants[restaurant_file[:-4]] = backend.Restaurant(
+                restaurant_file[:-4], True, restaurant_file)
 
         print("Here is all the restaurants we have:")
         for index, restaurant in enumerate(restaurants.keys(), 1):
             print(f"{index}. {restaurant}")
-        number = input("Enter one of the restaurants(number only), or q to quit")
+        number = input(
+            "Enter one of the restaurants(number only), or q to quit")
         number = "q" if number == "q" else int(number) - 1
         if number == "q":
             break
@@ -26,5 +34,6 @@ def main():
             print("The value is invalid, please try again.")
         else:
             restaurants[restaurant_selected].display_foods(console)
-            console.print("\n\nEnter q to go back to the restaurants", justify="center")
+            console.print("\n\nEnter q to go back to the restaurants",
+                          justify="center")
             backend.quit_no_input()
