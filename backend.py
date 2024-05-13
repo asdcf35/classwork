02f0ba2723e5ffd02a6ec727dcbb0b8f7637651e
asdcf35 <prann1234556@gmail.com>
2024-05-13 10:31:33 +0000

    May 13, 2024, 6:31 AM

--- backend.py
+++ backend.py
@@ -55,19 +55,28 @@ def rides_from_file(filename="rides.csv", admin=False) -> dict[str, Ride]:
     dataframex = pd.read_csv(filename)
     #locate all the rows where Working is equal to true(working in t)
     dataframex = dataframex.loc[dataframex["Working"] == True]
+    #if there are any values that are not filled in, then fillna will fill them in
     dataframex.loc[:, "Max"] = dataframex.loc[:, "Max"].fillna(999999999)
+    #if there are any values that are not filled in, then fillna will fill them in
     dataframex.loc[:, "Min"] = dataframex.loc[:, "Min"].fillna(0)
+
+    #convert the dataframe to a list of dictionary
     dataframe = dataframex.to_dict("records")
+    #make a new dictionary called rides
     rides = {}
+    #for the dictionary in the dataframe
     for dictionary in dataframe:
+        #use the name as the key, and a Ride Object as the value
         rides[dictionary["Name"]] = Ride(
             dictionary["Name"],
             (dictionary["Min"], dictionary["Max"]),
             dictionary["Description"],
             dictionary["Working"],
         )
+    #if it it's not an admin, return only the rides object
     if admin == False:
         return rides
+    #else, return both the dataframe and the rides object
     else:
         return rides, dataframex
 
@@ -83,10 +92,18 @@ class Food_Item:
 class Restaurant:
 
     def __init__(self, name, open, filename) -> None:
+        """Initalize the restaurant, creating all the variables and reading the csv"""
+        #setup the variables
         self.name = name
         self.open = open
+
+        #read the csv file and turn into a pandas DataFrame
         self.dataframe = pd.read_csv(filename)
+
+        #make another variable and store it in self.item_names
         self.item_names = tuple(self.dataframe.loc[:, "Items"])
+
+        #store all the food_item objects in a list called self.items(the name the index in self.item_names, corresponds to the Food_Item object at the same index in self.items) 
         self.items = [
             Food_Item(
                 self.item_names[i],
@@ -97,9 +114,19 @@ class Restaurant:
         ]
 
     def display_foods(self, console, discount=0) -> None:
+        """displays all the food in an easy to read matter"""
         os.system("cls")
+        
+        #greet the user
         console.print(f"Welcome to {self.name}\n\n", justify="center")
+        
+        #print Menu along with a line
         console.print(f"Menu", justify="center")
         console.print(f'{"":-^30}', justify="center")
+
+        #get the lines of any dataframe that contains True(so all items that are available) and use only the Price column, and the Items 
         print_half = self.dataframe[(self.dataframe==True).any(axis=1)].loc['Price','Items']
+
+        #print the query(which creates a new dataframe) in a centered way
         console.print(print_half.to_string(index=False), justify="center")
+
